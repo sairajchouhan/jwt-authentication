a19b6228cd24d32294bc419bb78cea29473e9e58
sairaj <sairaj2119@gmail.com>
2021-05-09 20:34:06 +0530

    server: create a login mutation and return a jwt

--- server/src/UserResolver.ts
+++ server/src/UserResolver.ts
@@ -1,12 +1,43 @@
-import { Arg, Mutation, Query, Resolver } from 'type-graphql'
-import { hash } from 'bcryptjs'
+import { Arg, Field, Mutation, ObjectType, Query, Resolver } from 'type-graphql'
+import { hash, compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
 import { User } from './entity/User'
+
+@ObjectType()
+class LoginResponse {
+  @Field()
+  accessToken: string
+}
 
 @Resolver()
 export class UserResolver {
   @Query(() => [User])
   users() {
     return User.find()
+  }
+
+  @Mutation(() => LoginResponse)
+  async login(
+    @Arg('email') email: string,
+    @Arg('passowrd') password: string
+  ): Promise<LoginResponse> {
+    const user = await User.findOne({ where: { email } })
+
+    if (!user) {
+      throw new Error('could not find user')
+    }
+
+    const valid = await compare(password, user.password)
+    if (!valid) {
+      throw new Error('bad passoword')
+    }
+
+    // login successful
+    return {
+      accessToken: sign({ userId: user.id }, 'asleghpqwouernga', {
+        expiresIn: '15m',
+      }),
+    }
   }
 
   @Mutation(() => Boolean)
